8a5463a018dfcb5013ed8d72f7ac3e6bc8ec1818
Josh <jmaxwel@sandia.gov>
2019-09-19 10:21:51 -0600

    adding version print to update script

--- update-nvim.sh
+++ update-nvim.sh
@@ -1,13 +1,11 @@
-pushd ~/bin
+pushd $HOME/bin >> /dev/null
 
 # Download latest stable release
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 chmod u+x ./nvim.appimage
 
-# Update system alternatives to use neovim
-# sudo update-alternatives --install /usr/bin/vi vi /home/jmaxwel/bin/nvim.appimage 60
-# sudo update-alternatives --install /usr/bin/vim vim /home/jmaxwel/bin/nvim.appimage 60
-# sudo update-alternatives --install /usr/bin/editor editor /home/jmaxwel/bin/nvim.appimage 60
+echo 'Current version:'
+./nvim.appimage --version | head -n 1
 
-popd
+popd >> /dev/null
 
