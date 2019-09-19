8892d8493c8b650bb3c6d8aba7c3987ed2c76e1f
Josh <jmaxwel@sandia.gov>
2019-09-19 10:14:04 -0600

    replacing ~ with /home/jmaxwel

--- install-nvim.sh
+++ install-nvim.sh
@@ -1,23 +1,35 @@
 echo 'Setting up directories'
-mkdir -p ~/.config/nvim
-mkdir -p ~/bin/
+mkdir -p $HOME/.config/nvim
+mkdir -p $HOME/bin/
 
 echo 'Installing dependencies'
 sudo apt update
 sudo apt install curl xclip python3-pip
 pip3 install pynvim
 
-pushd ~/bin
+pushd $HOME/bin
 echo 'Downloading latest stable neovim'
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 chmod u+x ./nvim.appimage
 popd
 
 echo 'Updating system alternatives to use neovim'
-sudo update-alternatives --install /usr/bin/vi vi /home/jmaxwel/bin/nvim.appimage 60
-sudo update-alternatives --install /usr/bin/vim vim /home/jmaxwel/bin/nvim.appimage 60
-sudo update-alternatives --install /usr/bin/editor editor /home/jmaxwel/bin/nvim.appimage 60
+sudo update-alternatives --install /usr/bin/vi vi $HOME/bin/nvim.appimage 60
+sudo update-alternatives --install /usr/bin/vim vim $HOME/bin/nvim.appimage 60
+sudo update-alternatives --install /usr/bin/editor editor $HOME/bin/nvim.appimage 60
+
+echo 'Adding nvim alias (reload required)'
+echo "alias nvim='vim'" >> ~/.bash_aliases
 
 echo 'Installing vim-plug'
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo 'Installing / Updating plugins from init.nvim'
+nvim -c ':PlugInstall' -c ':UpdateRemotePlugins' -c ':qall'
+
+echo 'Checking the health of current nvim configuration'
+nvim -c ':checkhealth'
+
+echo 'Done! Enjoy Neovim!"
+exit 0
